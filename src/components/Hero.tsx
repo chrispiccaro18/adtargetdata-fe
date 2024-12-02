69f7541282be4682c48a1c16d03652f02971f5cc
Chris Piccaro <chrispiccaro18@gmail.com>
2024-12-01 19:09:56 -0500

    quick updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,22 +10,20 @@ export const Hero = () => {
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              Shadcn
+              Optimize
             </span>{" "}
-            landing page
+            Your Digital
           </h1>{" "}
-          for{" "}
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              React
+              Ad
             </span>{" "}
-            developers
+            Spend
           </h2>
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Build your React landing page effortlessly with the required sections
-          to your project.
+        Simplify your ad campaigns with data-driven insights and actionable recommendations.
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
